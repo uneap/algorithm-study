3cd4cd5bd6256f32515386086711f6a51c3f0277
uneap@sk.com <wjdekdms9@sookmyung.ac.kr>
2024-05-27 23:49:08 +0900

    1주차- 브루트포스 설명

--- brute-force/src/완전탐색_피보나치수열.java
+++ brute-force/src/완전탐색_피보나치수열.java
@@ -3,15 +3,19 @@ import static java.lang.Integer.parseInt;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
-public class BruteForce_10870 {
-    //숫자 입력
+public class 완전탐색_피보나치수열 {
+    //숫자 입력받기
     public static int solutionInt() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         return parseInt(br.readLine());
 
     }
+    // 0 1 1 2 3 5 8 ...
+    // f(0) -> 0
+    // f(1) -> 1
+    // f(2) -> f(1) + f(0) = 1
+    // f(3) -> f(1) + f(2) = 2
     public static int fibonacci(int num) {
         if(num <= 0) {
             return 0;
