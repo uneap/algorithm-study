bcdccb9ddd3454a1f2357049e385417e30c7a920
uneap@sk.com <wjdekdms9@sookmyung.ac.kr>
2024-06-17 23:00:01 +0900

    4주차- dp

---
+++ dp/src/main/java/com/financial/dp_피보나치수.java
@@ -0,0 +1,21 @@
+package com.financial;
+
+import static java.lang.Integer.parseInt;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class dp_피보나치수 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = parseInt(br.readLine());
+        long[] fibonacci = new long[num + 1];
+        fibonacci[0] = 0;
+        fibonacci[1] = 1;
+        for(int i = 2; i <= num; i++) {
+            fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+        }
+        System.out.println(fibonacci[num]);
+    }
+}
