427aea3edbeb91e35b6c0c54cd7a897807f271e7
uneap@sk.com <wjdekdms9@sookmyung.ac.kr>
2024-07-08 00:01:39 +0900

    1주차 - 브루트포스

--- brute-force/src/n_queen.java
+++ brute-force/src/n_queen.java
@@ -5,44 +5,39 @@ import java.io.IOException;
 public class n_queen {
 
     public static int count = 0;
-
+    // 모든 퀸을 다 놓아야 함.
+    // 말을 놓을 때마다 값이 변경됨.
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
-
-        nQueen(arr,0);
+        int[] rows = new int[n];
+        dfs(rows, 0);
         System.out.println(count);
-
     }
 
-    public static void nQueen(int[] arr, int depth) {
-        if (depth == arr.length) {
+    public static void dfs(int[] rows, int a) {
+        if(a == rows.length){
             count++;
             return;
         }
-
-        for (int i = 0; i < arr.length; i++) {
-            arr[depth] = i;
-            if (possibility(arr, depth)) {
-                nQueen(arr,depth + 1);
+        for(int i = 0; i < rows.length; i++) {
+            rows[a] = i;
+            if(isPossible(rows, a)) {
+                dfs(rows,a + 1);
             }
         }
-
     }
 
-    public static boolean possibility(int[] arr, int col) {
-        for (int i = 0; i < col; i++) {
-            if (arr[col] == arr[i]) {
+    public static boolean isPossible(int[] rows, int a) {
+        for(int b = 0; b < a; b++) {
+            if (rows[a] == rows[b]) {
                 return false;
             }
-
-            else if (Math.abs(col - i) == Math.abs(arr[col] - arr[i])) {
+            if (Math.abs(a - b) == Math.abs(rows[a] - rows[b])) {
                 return false;
             }
         }
-
         return true;
     }
 }
