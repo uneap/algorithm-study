051d89d522e56062356b5536f06f163795c556ab
Jeongdaeun <wjdekdms9@sookmyung.ac.kr>
2024-09-02 21:14:03 +0900

    Time: 190 ms (30.69%) | Memory: 42.3 MB (75.87%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -3,16 +3,15 @@ class Solution {
     public String longestPalindrome(String s) {
         for(int i = s.length(); i >= 0; i --) {
             for(int j = 0; j + i <= s.length(); j++) {
-                String str = s.substring(j, i + j);
                 boolean isPalindrome = true;
                 for(int k = 0; k < i / 2; k++) {
-                    if(str.charAt(k) != str.charAt(str.length() - 1 - k)) {
+                    if(s.charAt(j + k) != s.charAt(i + j - 1 - k)) {
                         isPalindrome = false;
                         break;
                     }
                 }
                 if(isPalindrome) {
-                    return str;
+                    return s.substring(j, i + j);
                 }
             }
         }
