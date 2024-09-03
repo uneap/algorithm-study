c41f92ffd0d3b5febc854a5b71bf96662cb95c68
Jeongdaeun <wjdekdms9@sookmyung.ac.kr>
2024-09-04 00:17:53 +0900

    Time: 36 ms (16.28%) | Memory: 45.4 MB (85.92%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,20 +1,20 @@
 class Solution {
     // 순회
-    // 다른 순회하는 인덱스는 중첩 인덱스 까지만 순회하면서 
+    // 갈수 있는지 여부를 dp에 저장
+    // 갈수 있으면 true 후 break
     public boolean canJump(int[] nums) {
-        int dp[] = new int[nums.length];
-        Arrays.fill(dp, 10000);
+        boolean dp[] = new boolean[nums.length];
         if(nums[0] != 0 || nums.length == 1) {
-            dp[0] = 0;
+            dp[0] = true;
         }
-        for(int i = 0; i < nums.length - 1; i++) {
-            for(int j = 0; j <= i + nums[i]; j++) {
-                if(j >= nums.length) {
+        for(int i = 1; i < nums.length; i++) {
+            for(int j = i - 1; j >= 0; j--) {
+                if(dp[j] && nums[j] + j >= i) {
+                    dp[i] = true;
                     break;
                 }
-                dp[j] = Math.min(dp[j], dp[i] + 1);
             }
         }
-        return dp[nums.length - 1] < nums.length;
+        return dp[nums.length - 1];
     }
 }
