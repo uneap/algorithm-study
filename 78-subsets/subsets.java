e0217034b8546e51009d54b13d9638c9b9ebc867
Jeongdaeun <wjdekdms9@sookmyung.ac.kr>
2024-09-18 20:33:50 +0900

    Time: 0 ms (100.00%) | Memory: 42.9 MB (20.87%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,17 +1,16 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
+        int subsetSize = 1 << nums.length;
         List<List<Integer>> answers = new ArrayList<>();
-        make(answers, new LinkedList<>(), nums, 0);
-        return new ArrayList<>(answers);
-    }
-    
-    public void make(List<List<Integer>> answers, LinkedList<Integer> answer, int[] nums, int i) {
-        answers.add(new ArrayList<>(answer));
-        for(int index = i; index < nums.length; index++){
-            answer.add(nums[index]);
-            make(answers, answer, nums, index + 1);
-            answer.removeLast();
+        for(int i = 0; i < subsetSize; i++) {
+            List<Integer> answer = new ArrayList<>();
+            for(int j = 0; j < nums.length; j++) {
+                if((i & 1 << j) != 0) {
+                    answer.add(nums[j]);   
+                }
+            }
+            answers.add(answer);
         }
-        
+        return answers;
     }
 }
