c67b7bb304fee57390ac73ca041e4727162a96c6
Jeongdaeun <wjdekdms9@sookmyung.ac.kr>
2024-09-18 20:52:26 +0900

    Time: 4 ms (16.00%) | Memory: 43.9 MB (10.41%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -2,18 +2,15 @@ class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Set<List<Integer>> answers = new HashSet<>();
         Arrays.sort(nums);
-        combination(answers, new LinkedList<>(), 0, nums);
+        for(int i = 0; i < 1 << nums.length; i++) {
+            List<Integer> answer = new ArrayList<>();
+            for(int j = 0; j < nums.length; j++) {
+                if((i & 1 << j) != 0) {
+                    answer.add(nums[j]);
+                }
+            }
+            answers.add(answer);
+        }
         return new ArrayList<>(answers);
-    }
-    public void combination(Set<List<Integer>> answers, LinkedList<Integer> answer, int i,int[] nums) {
-        answers.add(new ArrayList<>(answer));
-        if(i >= nums.length) {
-            return;
-        }
-        for(int index = i; index < nums.length; index++) {
-            answer.add(nums[index]);
-            combination(answers, answer, index + 1, nums);
-            answer.removeLast();
-        }
     }
 }
