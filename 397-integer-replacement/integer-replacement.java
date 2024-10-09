043653e26de066bed52d23110a0d77391ba724b7
Jeongdaeun <wjdekdms9@sookmyung.ac.kr>
2024-10-09 14:56:51 +0900

    Time: 6 ms (19.73%) | Memory: 39.9 MB (98.38%) - LeetSync

--- 397-integer-replacement/integer-replacement.java
+++ 397-integer-replacement/integer-replacement.java
@@ -1,17 +1,16 @@
-import java.math.*;
 class Solution {
     public int integerReplacement(int n) {
-        return convert(new BigInteger(Integer.toString(n)), 0);
+        return convert((long)n, 0);
     }
     
-    public int convert(BigInteger n, int count) {
-        if(n.equals(new BigInteger("1"))) {
+    public int convert(long n, int count) {
+        if(n == 1) {
             return count;
         }
-        if(n.remainder(new BigInteger("2")).equals(new BigInteger("0"))) {
-            return convert(n.divide(new BigInteger("2")), count + 1);
+        if(n % 2 == 0) {
+            return convert(n / 2, count + 1);
         } else {
-            return Math.min(convert(n.add(new BigInteger("1")), count + 1), convert(n.subtract(new BigInteger("1")), count + 1));
+            return Math.min(convert(n + 1, count + 1), convert(n - 1, count + 1));
         }
     }
 }
