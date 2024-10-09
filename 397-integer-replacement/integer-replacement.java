05e221efeca0bfae2e35e1d514d0042a35a1967d
Jeongdaeun <wjdekdms9@sookmyung.ac.kr>
2024-10-09 15:03:37 +0900

    Time: 1 ms (59.18%) | Memory: 40.6 MB (21.47%) - LeetSync

--- 397-integer-replacement/integer-replacement.java
+++ 397-integer-replacement/integer-replacement.java
@@ -1,16 +1,22 @@
 class Solution {
     public int integerReplacement(int n) {
-        return convert((long)n, 0);
+        Map<Long, Integer> dp = new HashMap<>();
+        dp.put(1L, 0);
+        replacement(n, dp);
+        return dp.get(Long.valueOf(n));
     }
-    
-    public int convert(long n, int count) {
+    public int replacement(long n, Map<Long, Integer> dp) {
+        if(dp.containsKey(n)) {
+            return dp.get(n);
+        }
         if(n == 1) {
-            return count;
+            return 0;
         }
-        if(n % 2 == 0) {
-            return convert(n / 2, count + 1);
+        if(n % 2 == 1) {
+            dp.put(n, Math.min(dp.getOrDefault(n, Integer.MAX_VALUE), Math.min(replacement(n + 1, dp) + 1, replacement(n - 1, dp) + 1)));
         } else {
-            return Math.min(convert(n + 1, count + 1), convert(n - 1, count + 1));
+            dp.put(n, Math.min(dp.getOrDefault(n, Integer.MAX_VALUE), replacement(n / 2, dp) + 1));
         }
+        return dp.get(n);
     }
 }
