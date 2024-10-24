97960c2ff48b34801af44bf2ec2b32b220260131
Jeongdaeun <wjdekdms9@sookmyung.ac.kr>
2024-10-24 15:53:11 +0900

    Time: 5 ms (75.95%) | Memory: 54.8 MB (82.88%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,11 +1,12 @@
+// 사는 가격과 파는 가격 최댓값을 모두 측정
 class Solution {
     public int maxProfit(int[] prices, int fee) {
-        int buy = Integer.MIN_VALUE;
-        int sell = 0;
-        for(int price : prices) {
-            buy = Math.max(buy, sell - price);
-            sell = Math.max(sell, buy + price - fee);
+        int sell = Integer.MIN_VALUE;
+        int buy = 0;
+        for(int i = 0; i < prices.length; i++) {
+            sell = Math.max(sell, buy - prices[i]);
+            buy = Math.max(buy, sell + prices[i] - fee);
         }
-        return sell;
+        return buy;
     }
 }
