3ef71e7b1500692efd04a70dd98938b7d1871bce
Jeongdaeun <wjdekdms9@sookmyung.ac.kr>
2024-10-26 22:08:38 +0900

    Time: 0 ms (100.00%) | Memory: 42.1 MB (22.26%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
@@ -1,18 +1,17 @@
-// mid에서 right범위에 있느냐 left에서 Mid 범위에 있느냐
-// right와 mid를 비교해야 범위선정이 가능함
 class Solution {
     public int findMin(int[] nums) {
-        int left = 0; int right = nums.length - 1;
         int answer = Integer.MAX_VALUE;
-        while(left < right) {
+        int left = 0;
+        int right = nums.length - 1;
+        while(left <= right) {
             int mid = (left + right) / 2;
-            answer = Math.min(answer, nums[mid]);
-            if(nums[mid] < nums[right]) {
-                right = mid;
+            answer = Math.min(nums[mid], answer);
+            if(nums[right] > nums[mid]) {
+                right = mid - 1;
             } else {
                 left = mid + 1;
             }
         }
-        return Math.min(answer, nums[left]);
+        return answer;
     }
 }
